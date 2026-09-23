61078b099e46f3a5ac9116b185ed27d03cb19b4f
vitprofessional <46179916+vitprofessional@users.noreply.github.com>
2026-09-23 18:12:54 +0600

    Update index.php

--- index.php
+++ index.php
@@ -5,16 +5,15 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
-// Determine if the application is in maintenance mode...
-if (file_exists($maintenance = __DIR__.'/storage/framework/maintenance.php')) {
+$applicationPath = dirname(__DIR__, 2).'/apps/tiaba-dreager';
+
+if (file_exists($maintenance = $applicationPath.'/storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
-// Register the Composer autoloader...
-require __DIR__.'/vendor/autoload.php';
+require $applicationPath.'/vendor/autoload.php';
 
-// Bootstrap Laravel and handle the request...
 /** @var Application $app */
-$app = require_once __DIR__.'/bootstrap/app.php';
+$app = require_once $applicationPath.'/bootstrap/app.php';
 
 $app->handleRequest(Request::capture());
